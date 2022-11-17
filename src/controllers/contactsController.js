1541eb40c0ee8c37f1b190a9f9d8f4c87f688d69
Yevhenii Kobets <eu.kobets@gmail.com>
2022-11-17 21:13:45 +0200

    fix errors

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -7,56 +7,56 @@ const {
     deleteContactById,
 } = require('../services/contactsService');
 
-const getContactsController = async (req, res) => {
-    const { _id: userId } = req.user;
+const { notFoundError } = require('../helpers/errors');
 
-    const contacts = await getContacts(userId);
+const getContactsController = async (req, res) => {
+    const contacts = await getContacts();
 
     res.status(200).json(contacts);
 };
 
 const getContactByIdController = async (req, res) => {
-    const { _id: userId } = req.user;
+    const contact = await getContactById(req.params.contactId);
 
-    const contact = await getContactById(req.params.contactId, userId);
+    if (!contact) throw notFoundError;
 
     res.status(200).json(contact);
 };
 
 const createContactController = async (req, res) => {
-    const { _id: userId } = req.user;
+    const createdContact = await createContact(req.body);
 
-    const createdContact = await createContact(req.body, userId);
+    if (!createdContact) throw notFoundError;
 
     res.status(201).json(createdContact);
 };
 
 const updateContactByIdController = async (req, res) => {
-    const { _id: userId } = req.user;
-
     const updatedContact = await updateContactById(
         req.params.contactId,
-        req.body,
-        userId
+        req.body
     );
+
+    if (!updatedContact) throw notFoundError;
 
     res.status(200).json(updatedContact);
 };
 
 const toggleFavoriteByIdController = async (req, res) => {
-    const { _id: userId } = req.user;
-
     const updatedContact = await toggleFavoriteById(
         req.params.contactId,
-        req.body,
-        userId
+        req.body
     );
+
+    if (!updatedContact) throw notFoundError;
 
     res.status(200).json(updatedContact);
 };
 
 const deleteContactByIdController = async (req, res) => {
-    await deleteContactById(req.params.contactId, userId);
+    const deletedContact = await deleteContactById(req.params.contactId);
+
+    if (!deletedContact) throw notFoundError;
 
     res.status(200).json({ message: 'Contact deleted' });
 };
--- src/services/contactsService.js
+++ src/services/contactsService.js
@@ -5,13 +5,11 @@ const getContacts = async userId => {
 };
 
 const getContactById = async (contactId, userId) => {
-    const contact = await Contact.findOne({ _id: contactId, userId });
-
-    if (!contact) {
-        const error = new Error('Not found');
-        error.status = 404;
-        throw error;
-    }
+    const contact = await Contact.findOne({ _id: contactId, userId }).catch(
+        () => {
+            return null;
+        }
+    );
 
     return contact;
 };
@@ -28,37 +26,23 @@ const updateContactById = async (contactId, body, userId) => {
         { _id: contactId, userId },
         body,
         { new: true }
-    );
-
-    if (!updatedContact) {
-        const error = new Error('Not found');
-        error.status = 404;
-        throw error;
-    }
+    ).catch(() => {
+        return null;
+    });
 
     return updatedContact;
 };
 
 const toggleFavoriteById = async (contactId, body, userId) => {
-    if (!body.favorite && !body.favorite === false) {
-        const error = new Error('Missing field favorite');
-        error.status = 400;
-        throw error;
-    }
-
     const updatedContact = await Contact.findOneAndUpdate(
         { _id: contactId, userId },
         body,
         {
             new: true,
         }
-    );
-
-    if (!updatedContact) {
-        const error = new Error('Not found');
-        error.status = 404;
-        throw error;
-    }
+    ).catch(() => {
+        return null;
+    });
 
     return updatedContact;
 };
@@ -67,13 +51,9 @@ const deleteContactById = async (contactId, userId) => {
     const deletedContact = await Contact.findOneAndRemove({
         _id: contactId,
         userId,
+    }).catch(() => {
+        return null;
     });
-
-    if (!deletedContact) {
-        const error = new Error(`Not found`);
-        error.status(404);
-        throw error;
-    }
 
     return deletedContact;
 };
